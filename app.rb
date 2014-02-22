fb662e81aac9c4e9deca53bca8172f917a53701f
Anders Fisher <atleastimtrying@gmail.com>
2014-02-22 12:38:48 +0000

    commented everything out so far, such a hack on a hack day!

--- app.rb
+++ app.rb
@@ -1,8 +1,44 @@
 require 'sinatra'
 require 'json'
-require 'net/http'
 require 'uri'
 require 'pry'
+require 'pg'
+require 'data_mapper'
+require 'sanitize'
+# DataMapper.setup(:default, ENV['HEROKU_POSTGRESQL_VIOLET_URL'] || ENV['HEROKU_POSTGRESQL_TEAL_URL'] || ENV['LOCAL_URL'])
+
+# helpers do
+#   def protected!
+#     return if authorized?
+#     headers['WWW-Authenticate'] = 'Basic realm="Restricted Area"'
+#     halt 401, "Not authorized\n"
+#   end
+
+#   def authorized?
+#     @auth ||=  Rack::Auth::Basic::Request.new(request.env)
+#     @auth.provided? and @auth.basic? and @auth.credentials and @auth.credentials == [ENV['UN'], ENV['PWD']]
+#   end
+# end
+
+# class Company
+#   include DataMapper::Resource
+#   property :id, Serial 
+#   property :company_name, String
+#   property :mission_statement, Text
+#   property :mission_statement_proof, Text
+#   property :mission_statement_investigator, String
+#   property :time, String
+# end
+
+# class NewsSource
+#   include DataMapper::Resource
+#   property :id, Serial
+#   property :company_id, Integer
+#   property :name, String
+#   property :headline, String
+#   property :polarity, Float  
+# end  
+
 
 get '/' do
   send_file 'sample.html'
@@ -16,19 +52,23 @@ get '/style.css' do
   send_file 'style.css'
 end
 
-# get '/favicon.ico' do
-#   send_file 'favicon.ico'
+# get '/company/new' do
+#   protected!
+#   send_file 'new_company.html'
+# end
+
+# post '/company' do
+#   protected!
+
+# end
+
+# post '/news_source/new' do
+#   protected!
+
 # end
 
 get '/sample.json' do
-  # uri = URI.parse("http://pod.opendatasoft.com/api/records/1.0/search")
-  # response = Net::HTTP.post_form(uri, {
-  #   dataset: "pod_brand",
-  #   facet: "brand_nm", 
-  #   facet: "owner_nm"
-  # })
-  # json = JSON.parse(response.body)
-   {
+  [{
     company_name: 'Coca-Cola' || params[:company_name],
     mission_statement: 'Coca-Cola is awesome x approx 150 words',
     mission_statement_proof: 'Coca cola IS awesome for realz',
@@ -45,5 +85,5 @@ get '/sample.json' do
         polarity: 0.9
       },
     ]
-  }.to_json
+  }].to_json
 end
